f242ecdf3cb2948a8761dc0f0260b312137084cb
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-03-03 08:21:52 -0500

    Create decteccion_intrusos_sensores.java

---
+++ decteccion_intrusos_sensores.java
@@ -0,0 +1,22 @@
+package deteccion_intrusos_sensores;
+
+import java.util.Scanner;
+
+
+public class deteccion_intrusos_sensores {
+ 
+    public static void main(String[] args){
+        Scanner TomarDatos = new Scanner(System.in);
+        System.out.print("intruso detectado");
+        int deteccion = TomarDatos.nextInt();
+        TomarDatos.close();
+        if(deteccion)int int=10
+        { 
+            System.out.println("atencion, intrusos se esta acecando");
+        }
+        else{
+                System.out.println("atencion, el intruso esta en la entrada");
+                }
+        return null;
+    }
+}
