005f9a4008e360cc37805157c538ed40e3e43275
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-03-06 08:01:01 -0500

    Create Aire acondicionado.java

---
+++ Aire acondicionado.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+public class aire_acondicionado {
+    public static void Temperatura(){
+        Scanner Temperatura = new Scanner(System.in);
+        Objtemp.temperatura = "Aire";
+        int temp;
+        System.out.print("ingrese la temperatura c°:");
+        if(temp>28°){
+        System.out.println("Activado");
+    }
+        else{
+                System.out.println("desactivado");
+                }
+        
+    }
+    public static void humedad(){
+        int humedad = 0;
+        objtemp.humeda="Aire";
+        if(humedad>60%){
+            System.out.println("Activado");
+        }
+        else{
+            System.out.print("desactivado");
+        }
+    }
+    public static void Sensor(){
+        boolean = true;
+        while(tem>30°=humedad){
+        if(true){
+            System.out.print("luz amarilla encendido");
+        }
+        else(false){
+            System.out.print("luz roja apagado");
+        }
+        break;
+    }
+    }
+}
